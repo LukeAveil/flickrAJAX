e583b721707958238e807cfe8f133440e01d954a
Luke <lukeaveil@yahoo.com>
2016-11-10 17:00:01 +0000

    adds functionality to bring in photos to html

--- js/flickr.js
+++ js/flickr.js
@@ -10,7 +10,14 @@ $('button').click(function() {
   };
 
   function displayPhotos(data) {
-
+    var photoHTML = '<ul>';
+    $.each(data.items, function(i, photo) {
+      photoHTML += '<li class="grid-25 tablet-grid-50">';
+      photoHTML += '<a href="' + photo.link + '" class="image">';
+      photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+    });
+    photoHTML += '</ul>';
+    $("#photos").html(photoHTML);
   }
 
   $.getJSON(flickrAPI, flickrOptions, displayPhotos);
